9f633432b89c211f6b8eb66c614b3175f849cf49
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2022-03-04 20:01:25 -0300

    aula
    aula1

---
+++ js/atividade1.js
@@ -0,0 +1,11 @@
+// 1 - se a peça tiver mais que 100g, pode cadastrar.
+
+// var listaDePecas = ['Motor', 'filtro de Ar', 'lanterna'];
+let peso = 120;
+
+if(peso < 100){
+    console.log('A peça deve passar no minimo 100g')
+
+}else{
+    console.log('Aceita')
+}
